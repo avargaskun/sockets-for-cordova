e02802a85c624627302feb1044272671593da911
Lars Støttrup Nielsen <larsstoettrup@gmail.com>
2016-10-11 16:06:21 +0200

    Added RFC compliant UUID generator

--- socket.js
+++ socket.js
@@ -32,7 +32,7 @@ function Socket() {
     this.onData = null;
     this.onClose = null;
     this.onError = null;
-    this.socketKey = guid();
+    this.socketKey = guid.generate();
 }
 
 Socket.prototype.open = function (host, port, success, error) {
@@ -183,17 +183,26 @@ Socket._copyToArray = function (array) {
     return outputArray;
 };
 
-var guid = (function () {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-
-    return function () {
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
-    };
+/**
+ * Fast UUID generator, RFC4122 version 4 compliant.
+ * @author Jeff Ward (jcward.com).
+ * @license MIT license
+ * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+ **/
+var guid = (function() {
+  var self = {};
+  var lut = []; for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+  self.generate = function() {
+    var d0 = Math.random()*0xffffffff|0;
+    var d1 = Math.random()*0xffffffff|0;
+    var d2 = Math.random()*0xffffffff|0;
+    var d3 = Math.random()*0xffffffff|0;
+    return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+      lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+      lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+      lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+  };
+  return self;
 })();
 
 // Register event dispatcher for Windows Phone
